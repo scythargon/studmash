b89781c1a47c2202cc7d3ae498f55e836aca9fba
S. Argon <argon@argon-N550JV.(none)>
2014-01-31 15:36:44 +0700

    added south

--- studmash/studmash/settings.py
+++ studmash/studmash/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,9 +58,13 @@ WSGI_APPLICATION = 'studmash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE':'django.db.backends.postgresql_psycopg2',
+            'NAME': 'studmash',
+            'USER': 'studmash',
+            'PASSWORD': 'qweqweqwe',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
 }
 
 # Internationalization
